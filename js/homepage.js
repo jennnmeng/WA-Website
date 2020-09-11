83b04ebe0836e4a1ef5e553389a9b847a924b3f8
Jenniffer <jenniffer.meng2002@gmail.com>
2020-09-10 20:56:29 -0400

    formatted the navigation page, continue work on js

--- js/homepage.js
+++ js/homepage.js
@@ -1,24 +1,11 @@
 $(document).ready(function () {
 
-    var sectionSelected; // register section chosen by user
-
     /* when opening browser
 FADE IN: WA and subtitle (one after another)
 FADE IN: Blobs: section by section
 */
 
-
-/*
-    $("title_txt").css("display", "none");
-    $("subtitle_txt").css("display", "none")
-    $("title_txt").fadeIn(3000);
-    $("subtitle_txt").fadeIn(3000);
-
-*/
-
-
-    $('body').hide().fadeIn(4000);
-
+$('body').css('display','none').fadeIn(3000);
 
 
     // fade in sections based on user selection
@@ -29,9 +16,6 @@ FADE IN: Blobs: section by section
     b. load section on click
      */
 
-    $("h2").click(function () {
-        sectionSelected = $("h2").attr("id");
-    });
 
 });
 
