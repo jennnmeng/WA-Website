3a4681a54086e210bdaa3fa0f1cffbdbba12b7f2
Jenniffer <jenniffer.meng2002@gmail.com>
2020-09-29 22:56:53 -0400

    try add animations for nav page

--- js/homepage.js
+++ js/homepage.js
@@ -47,6 +47,79 @@ hometl.fromTo(
         { opacity: '100%', ease: Power2.easeInOut },
     )
 
+const foreword1 = document.querySelector("#foreword1");
+const foreword2 = document.querySelector("#foreword2");
+const wbl = document.querySelector("#wbl");
+const mtl = document.querySelector("#mtl");
+const cgs = document.querySelector("#cgs");
+
+const ppr = document.querySelector("#ppr");
+const ff = document.querySelector("#ff");
+const newmat = document.querySelector("#new");
+const purchase = document.querySelector("#purchase");
+
+const navtl = new TimelineMax();
+
+navtl.fromTo(
+    foreword1, foreword2,
+    1.5,
+    { opacity: '0%' },
+    { opacity: '100%', ease: Power2.easeInOut }
+)
+    .fromTo(
+        purchase,
+        1.5,
+        { opacity: '0%' },
+        { opacity: '100%', ease: Power2.easeInOut },
+        "-=1"
+    )
+    .fromTo(
+        wbl,
+        1,
+        { opacity: '0%' },
+        { opacity: '100%', ease: Power2.easeInOut },
+        "+=1"
+    )
+    .fromTo(
+        mtl,
+        1,
+        { opacity: '0%' },
+        { opacity: '100%', ease: Power2.easeInOut },
+        "-=0.5"
+    )
+    .fromTo(
+        cgs,
+        1,
+        { opacity: '0%' },
+        { opacity: '100%', ease: Power2.easeInOut },
+        "-=0.5"
+    )
+    .fromTo(
+        ppr,
+        1,
+        { opacity: '0%' },
+        { opacity: '100%', ease: Power2.easeInOut },
+        "-=0.5"
+    )
+    .fromTo(
+        ff,
+        1,
+        { opacity: '0%' },
+        { opacity: '100%', ease: Power2.easeInOut },
+        "-=0.5"
+    )
+    .fromTo(
+        newmat,
+        1,
+        { opacity: '0%' },
+        { opacity: '100%', ease: Power2.easeInOut },
+        "-=0.5"
+    )
+
+
+
+
+
 
 
 
