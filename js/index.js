b706b2ad9e97df35d0770d4e018b3858a0d2bd49
Jenniffer <jenniffer.meng2002@gmail.com>
2020-10-10 23:27:08 -0400

    readjust blobs on homepage. continue: js animations with barba.

--- js/index.js
+++ js/index.js
@@ -3,8 +3,6 @@ const title = document.querySelector("#title_txt");
 const subtitle = document.querySelector("#subtitle_txt");
 const enter = document.querySelector("#enter_bttn");
 
-const redSlider = document.querySelector("red_slider");
-
 // animate title, subtitle and blobs on load
 const hometl = new TimelineMax();
 
@@ -35,5 +33,49 @@ hometl.fromTo(
         { opacity: '100%', ease: Power2.easeInOut },
     )
 
-    // open red rectangle on hover
+// open red rectangle on hover
+
+function pageTransition() {
+    var tl = gsap.timeline();
+    tl.fromTo("#redSlider", {
+        duration: 2,
+        scaleX: 1,
+        ease: "Expo.easeInOut;"
+    })
+    tl.to("#redSlider", {
+        duration: 2,
+        scaleX: 0,
+        ease: "Expo.easeInOut;"
+    })
+}
+
+function pageAnimation() {
+    var tl = gsap.timeline();
+
+}
+
+
+barba.init({
+    sync: true,
+
+    transitions: [{
+
+        async leave(data) {
+            const done = this.async();
+            pageTransition();
+            await delay(1500);
+            done();
+        },
+
+        async enter(data) {
+            pageAnimation();
+        }
+
+        async once(data) {
+            pageAnimation();
+        }
+
+    }]
     
+}
+)
